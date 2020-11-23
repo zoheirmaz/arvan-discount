bc8ac60c39491c2e6b9179428039c849b865dd19
zoheir <zoheirmaz.zm@gmail.com>
2020-11-23 21:31:15 +0330

    feat: add CouponRepository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,12 +2,18 @@
 
 namespace App\Providers;
 
+use App\Repositories as Repositories;
 use Illuminate\Support\ServiceProvider;
+use Infrastructure\Repositories\CouponRepositoryInterface;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
     public function register()
     {
 
+        $this->app->bind(
+            CouponRepositoryInterface::class,
+            Repositories\CouponRepository::class
+        );
     }
 }
---
+++ app/Repositories/CouponRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Entities\Coupon;
+use Illuminate\Support\Facades\Auth;
+use Infrastructure\Traits\QueryTrait;
+use Infrastructure\Traits\EntityRelationsTrait;
+use Infrastructure\Repositories\CouponRepositoryInterface;
+
+class CouponRepository implements CouponRepositoryInterface
+{
+    use EntityRelationsTrait, QueryTrait;
+
+    public function findById($id)
+    {
+        return Coupon::query()
+            ->with($this->couponRelations())
+            ->findOrFail($id);
+    }
+
+    public function create($data)
+    {
+        return Coupon::create([
+            Coupon::TITLE => $data['title'],
+            Coupon::TYPE => $data['type'],
+            Coupon::RULES => $this->uniqueData($data['rules'], 'rule'),
+            Coupon::RESULTS => $this->uniqueData($data['results'], 'result'),
+            Coupon::CREATED_BY => Auth::id(),
+        ]);
+    }
+
+    private function uniqueData(array $data, $key): array
+    {
+        return collect($data)->unique($key)->toArray();
+    }
+}
