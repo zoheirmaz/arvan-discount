0b97079bdce35da6989f73e54385f87a13679060
zoheir <zoheirmaz.zm@gmail.com>
2020-11-23 21:39:50 +0330

    feat: add CouponController and its routes

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 /**
  * @OA\Info(
  *      version="1.0.0",
- *      title="L5 OpenApi",
+ *      title="Coupon",
  *      description="L5 Swagger OpenApi description",
  *      @OA\Contact(
  *          email="zoheirmaz.zm@gmail.com"
@@ -29,76 +29,65 @@ use Illuminate\Support\Facades\Route;
  *  )
  */
 
+Route::prefix('/coupon')->group(function () {
+    /**
+     * @OA\Post(
+     *     path="/coupon/create",
+     *     summary="Login a user",
+     *     tags={"Coupon"},
+     *     @OA\RequestBody(
+     *          request="Pet",
+     *          description="create coupon input data structure",
+     *          required=true,
+     *          @OA\JsonContent(
+     *               @OA\Property(
+     *                  property="title",
+     *                  type="string",
+     *              ),
+     *              @OA\Property(
+     *                  property="type",
+     *                  type="integer",
+     *              ),
+     *              @OA\Property(
+     *                  property="rules",
+     *                  type="array",
+     *                  @OA\Items(
+     *                      @OA\Property(
+     *                          property="rule",
+     *                          type="integer",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="values",
+     *                          type="array",
+     *                          @OA\Items(
+     *                              type="string",
+     *                          ),
+     *                      ),
+     *                  ),
+     *              ),
+     *              @OA\Property(
+     *                  property="results",
+     *                  type="array",
+     *                  @OA\Items(
+     *                      @OA\Property(
+     *                          property="result",
+     *                          type="integer",
+     *                      ),
+     *                      @OA\Property(
+     *                          property="values",
+     *                          type="array",
+     *                          @OA\Items(
+     *                              type="string",
+     *                          ),
+     *                      ),
+     *                  ),
+     *              ),
+     *          )
+     *     ),
+     *     @OA\Response(response="200", description="login sucessful"),
+     *     @OA\Response(response="401", description="unauthorized")
+     * )
+     */
+    Route::post('/create', 'CouponController@create');
 
-/**
- * @OA\Get(
- *     path="/user",
- *     summary="Get current user",
- *     tags={"User"},
- *     @OA\Response(response="200", description="register sucessful"),
- *     @OA\Response(response="401", description="unauthorized"),
- *     security={
- *         {
- *             "bearer": {}
- *         }
- *     },
- * )
- */
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
 });
-
-/**
- * @OA\Post(
- *     path="/register",
- *     summary="Register a user",
- *     tags={"User"},
- *     @OA\RequestBody(
- *          description="Pass name, mobile, password",
- *          required=true,
- *          @OA\JsonContent(
- *              @OA\Property(
- *                  property="name",
- *                  type="string",
- *              ),
- *              @OA\Property(
- *                  property="mobile",
- *                  type="string",
- *              ),
- *              @OA\Property(
- *                  property="password",
- *                  type="string",
- *              )
- *          )
- *     ),
- *     @OA\Response(response="200", description="register sucessful"),
- *     @OA\Response(response="401", description="unauthorized")
- * )
- */
-Route::post('/register', 'AuthController@register');
-
-/**
- * @OA\Post(
- *     path="/login",
- *     summary="Login a user",
- *     tags={"User"},
- *     @OA\RequestBody(
- *          request="Pet",
- *          description="Pet object that needs to be added to the store",
- *          required=true,
- *          @OA\JsonContent(
- *              @OA\Property(
- *                  property="mobile",
- *                  type="string",
- *              ),
- *              @OA\Property(
- *                  property="password",
- *                  type="string",
- *              )
- *          )
- *     ),
- *     @OA\Response(response="200", description="login sucessful"),
- *     @OA\Response(response="401", description="unauthorized")
- * )
- */
-Route::post('/login', 'AuthController@login');
