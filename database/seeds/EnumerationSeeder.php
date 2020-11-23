6554258506fc8a2ffe4212a98d015f3c65cf4dec
zoheir <zoheirmaz.zm@gmail.com>
2020-11-23 21:40:30 +0330

    feat: add EnumerationSeeder

--- database/seeds/EnumerationSeeder.php
+++ database/seeds/EnumerationSeeder.php
@@ -14,36 +14,12 @@ class EnumerationSeeder extends Seeder
     {
         Enumeration::create([
             Enumeration::ID => 1,
-            Enumeration::TITLE => 'payment gateways'
+            Enumeration::TITLE => 'coupon rules'
         ]);
 
         Enumeration::create([
             Enumeration::ID => 2,
-            Enumeration::TITLE => 'zarinpal',
-            Enumeration::PARENT_ID => 1,
-        ]);
-
-        Enumeration::create([
-            Enumeration::ID => 3,
-            Enumeration::TITLE => 'transaction status'
-        ]);
-
-        Enumeration::create([
-            Enumeration::ID => 4,
-            Enumeration::TITLE => 'waiting',
-            Enumeration::PARENT_ID => 3,
-        ]);
-
-        Enumeration::create([
-            Enumeration::ID => 5,
-            Enumeration::TITLE => 'paid',
-            Enumeration::PARENT_ID => 3,
-        ]);
-
-        Enumeration::create([
-            Enumeration::ID => 6,
-            Enumeration::TITLE => 'failed',
-            Enumeration::PARENT_ID => 3,
+            Enumeration::TITLE => 'coupon results',
         ]);
     }
 }
