5fb6ca83d5b226a9c4561ca835e3c0a080481f31
zoheir <zoheirmaz.zm@gmail.com>
2020-11-25 20:48:17 +0330

    fix: change type of mobile to numeric in UsedCount validation

--- app/Validations/CouponRule/UsedCount.php
+++ app/Validations/CouponRule/UsedCount.php
@@ -11,7 +11,7 @@ class UsedCount extends ValidationAbstract
         return [
             'mobile' => [
                 'required',
-                'string',
+                'numeric',
             ],
         ];
     }
