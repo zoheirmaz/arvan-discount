8f8b4f65c30acda52af10712cd5ae386d5d260c4
zoheir <zoheirmaz.zm@gmail.com>
2020-11-25 20:40:27 +0330

    fix: add CouponUsage repository to RepositoryServiceProvider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Repositories as Repositories;
 use Illuminate\Support\ServiceProvider;
 use Infrastructure\Repositories\CouponRepositoryInterface;
+use Infrastructure\Repositories\CouponUsageRepositoryInterface;
 use Infrastructure\Repositories\EnumerationRepositoryInterface;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(
             CouponRepositoryInterface::class,
             Repositories\CouponRepository::class
+        );
+
+        $this->app->bind(
+            CouponUsageRepositoryInterface::class,
+            Repositories\CouponUsageRepository::class
         );
     }
 }
